54eb0309ceda4e31d2b4176737c22afe88f69d76
alfacentaury <alfa.arnialfa@gmail.com>
2019-04-12 13:54:04 +0700

    fixed database

--- index.php
+++ index.php
@@ -1,86 +1,103 @@
 <!DOCTYPE html>
-<html>
-<head>
+<html lang="en">
+  <head>
+
     <title>Submission Satu</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-</head>
-<body>
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+  </head>
+
+  <body>
     <div class="container">
-        <h1 class="text-center">Register Azure Development</h1>
-        <form action="index.php" method="POST">
-            <div class="form-group">
-                <label for="name">Name:</label>
-                <input type="text" class="form-control" name="name" id="name" require="">
-            </div>
-            <div class="form-group">
-                <label for="email">Email:</label>
-                <input type="text" class="form-control" name="email" id="email">
-            </div>
-            <div class="form-group">
-                <label for="jobs">Job:</label>
-                <input type="text" class="form-control" name="jobs" id="jobs">
-            </div>
-            <button type="submit" name="register" class="btn btn-primary">Register</button>
-        </form>
-        <br>
-        <br>
-        <br>
+      <h1 class="text-center">Register Azure Development</h1>
+    </div>
+    <div class="container">
         <div>
-        <table class="table table-hover table-bordered">
+          <form action="index.php" method="post">
+            <div class="form-group">
+              <label for="name">Name:</label>
+              <input type="text" class="form-control" name="name" id="name">
+            </div>
+            
+            <div class="form-group">
+              <label for="email">Email:</label>
+              <input type="email" class="form-control" name="email" id="email">
+            </div>
+            
+            <div class="form-group">
+              <label for="jobs">Jobs:</label>
+              <input type="text" class="form-control" name="jobs" id="jobs">
+            </div>
+           
+            <div class="form-group"> 
+                <button type="submit" name="register" class="btn btn-primary">Register</button>
+            </div>
+            
+          </form>
+        </div>
+        
+        <div>
+          <br>
+          <br>
+          <br>
+          <table class="table table-hover table-bordered">
             <thead>
-                <tr>
-                    <th>No</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Jobs</th>
-                </tr>
+              <tr>
+                <th>No</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Jobs</th>
+              </tr>
             </thead>
+            
             <tbody>
-                <?php
-                    include ("db_connection.php");
-                    try {
-                        $getdata = "SELECT * FROM [dbo].[tb_user]";
-                        $stmt = $conn->query($getdata);
-                        $data = $stmt->fetchAll();
-                        if(count($data) > 0) {
-                            $i = 1;
-                            foreach($data as $user) {
-                                echo "<tr><td>".$i++."</td>";
-                                echo "<td>".$user['name']."</td>";
-                                echo "<td>".$user['email']."</td>";
-                                echo "<td>".$user['jobs']."</td>";
-                            }
-                        } else {
-                            echo "<tr><td colspan=5 align=center>Data Empty</td></tr>";
-                        }
-                    } catch(Exception $e) {
-                        echo "Error: " . $e;
-                    }
-                ?>
+              <?php
+                  include ("dbconnection.php");
+                  try {
+                      $getdata = "SELECT * FROM tb_user ORDER BY id DESC";
+                      $stmt = $conn->query($getdata);
+                      $data = $stmt->fetchAll(); 
+                      if(count($data) > 0) {
+                          $i = 1;
+                          foreach($data as $tb_user) {
+                              echo "<tr><td>".$i++."</td>";
+                              echo "<td>".$tb_user['name']."</td>";
+                              echo "<td>".$tb_user['email']."</td>";
+                              echo "<td>".$tb_user['jobs']."</td>";
+                          }
+                          
+                      } else {
+                          echo "<tr><td align=center>Not found</td></tr>";
+                      }
+                  } catch(Exception $e) {
+                      echo "Error : " . $e;
+                  }
+              ?>
+              
             </tbody>
-        </table>
+          </table>
         </div>
     </div>
 
-    <?php
-        if(isset($_POST['register'])) {
-            try {
-                $name = $_POST['name'];
-                $email = $_POST['email'];
-                $jobs = $_POST['jobs'];
-
-                $insert_query = "INSERT INTO [dbo].[tb_user] (name, email, jobs) VALUES (?,?,?)";
-                
-                $stmt = $conn->prepare($insert_query);
-                $stmt->bindValue(1, $name);
-                $stmt->bindValue(2, $email);
-                $stmt->bindValue(3, $jobs);
-                $stmt->execute();
-                echo "<meta http-equiv='refresh' content='0'>";
-            } catch(Exception $e) {
-                echo "Error: " . $e;
-            }
+  <?php
+      if (isset($_POST['register'])) {
+        try {
+            $name = $_POST['name'];
+            $email = $_POST['email'];
+            $jobs = $_POST['jobs'];
+            
+            $insert_query = "INSERT INTO tb_user (name, email, jobs) 
+            VALUES (?,?,?)";
+            $stmt = $conn->prepare($insert_query);
+            $stmt->bindValue(1, $name);
+            $stmt->bindValue(2, $email);
+            $stmt->bindValue(3, $jobs);
+            $stmt->execute();
+            echo "<meta http-equiv='refresh' content='0'>";
+        } catch(Exception $e) {
+            echo "Error: " . $e;
         }
-    ?>
-</body>
+      }
+  ?>
+  </body>
 </html>
